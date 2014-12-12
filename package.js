3e2eb92a366ee3bf3daacb10ec52c3aca807e70b
PEM-- <pemarchandet@gmail.com>
2014-12-12 12:33:34 +0100

    Initial commit

---
+++ package.js
@@ -0,0 +1,37 @@
+Package.describe({
+  name: 'pierreeric:fview-rwdsimplemenu',
+  summary: 'Simple responsive menu plugin for famous-views',
+  version: '0.1.0',
+  git: 'https://github.com/PEM--/fview-rwdsimplemenu.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  // Both famo.us packages generally used in the Meteor community are
+  // included as weak references.
+  api.use('mjn:famous@0.3.1_2', 'client', { weak: true });
+  api.use('raix:famono@0.9.19', { weak: true });
+  // famous-views is integrated a mandatory reference.
+  api.use([
+    'coffeescript',
+    'blaze',
+    'tracker',
+    'underscore',
+    'iron:router@1.0.3',
+    'gadicohen:famous-views@0.1.29',
+    'mquandalle:jade@0.2.9',
+    'pierreeric:cssc@1.0.3',
+    'pierreeric:cssc-normalize@1.0.1',
+    'pierreeric:cssc-famous@1.0.2',
+    'pierreeric:cssc-colors@1.0.3'
+  ], 'client');
+  api.addFiles([
+    'fview-rwdsimplemenu.coffee'
+  ], 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('fview-rwdsimplemenu');
+  api.addFiles('fview-rwdsimplemenu-tests.js');
+});
